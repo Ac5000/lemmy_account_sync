8034ac35d7a1288f4105079fc68c7630da61c639
Ac5000 <15003210+Ac5000@users.noreply.github.com>
2023-07-01 01:01:56 -0400

    Adding unit tests

---
+++ tests/test_lemmy.py
@@ -0,0 +1,28 @@
+"""Unit tests for lemmy.py"""
+import os
+import unittest
+from pathlib import Path
+
+from lemmy_sync import get_accounts
+
+
+class TestLemmySync(unittest.TestCase):
+    """Main test case."""
+
+    def test_get_accounts(self):
+        """Test get_accounts function."""
+        expected_results = {'Main Account':
+                            {'site': 'https://sh.itjust.works',
+                             'user': 'Imauser',
+                             'password': 'apasswod'},
+                            'Account 2':
+                            {'site': 'https://lemmy.ml',
+                             'user': 'cooluser',
+                             'password': 'badpassword'}}
+
+        test_config = Path(os.path.dirname(__file__),
+                           'test_files',
+                           'testconfig.ini')
+        accounts = get_accounts(test_config)
+
+        self.assertEqual(accounts, expected_results)
