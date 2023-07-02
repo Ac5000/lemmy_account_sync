7991569291dc66574d5fd32a8b746b26e05a0b5f
Ac5000 <15003210+Ac5000@users.noreply.github.com>
2023-07-02 04:50:58 -0400

    Simplifying things to smaller pieces. Holding more information in the class itself. More focus on objects instead of dicts.

--- src/lemmy.py
+++ src/lemmy.py
@@ -1,153 +1,142 @@
 """Lemmy class for use in lemmy_migrate.py"""
+import json
 import sys
-from collections import defaultdict
-from time import sleep
+from types import SimpleNamespace
 from urllib.parse import urlparse
 
 import requests
 
+from account import Account
+from lem_types import MyUserInfo
 from log_config import configure_logging
 
 
-class Lemmy:
-    """Lemmy class for use in the migration."""
+class Instance:
+    """Class that pertains to one Lemmy instance."""
     _api_version = "v3"
     _api_base_url = f"api/{_api_version}"
 
-    def __init__(self, url: str) -> None:
-        parsed_url = urlparse(url)
+    def __init__(self, account: Account) -> None:
+        # Establish a logger based on the account.
+        self.logger = configure_logging(account.account)
+
+        self._auth_token = None
+        self.site_response = None
+        self.account = account
+
+        # Parse the URL for the site
+        parsed_url = urlparse(account.site)
         url_path = parsed_url.netloc if parsed_url.netloc else parsed_url.path
         self._site_url = urlparse(url_path)._replace(scheme='https',
                                                      netloc=url_path,
                                                      path='').geturl()
-        self._auth_token = None
-        self._user_communities = defaultdict(dict)
-        self.logger = configure_logging(url)
+        self.api_url = f'{self._site_url}/{self._api_base_url}'
 
-    def login(self, user: str, password: str) -> None:
-        """Authenticate to Lemmy instance. Generates self._auth_token
-
-        Args:
-            user (str): Username to use for login
-            password (str): Password to use for login
-        """
-        payload = {
-            'username_or_email': user,
-            'password': password
-        }
+    def login(self) -> None:
+        """Authenticate to Lemmy instance. Generates self._auth_token"""
+        payload = {'username_or_email': self.account.user,
+                   'password': self.account.password}
 
         self.logger.debug(f'Attempting to login to {self._site_url}')
 
         try:
-            resp = self._request_it(
-                f"{self._site_url}/{self._api_base_url}/user/login",
-                method='POST', json=payload)
-            self._auth_token = resp.json()['jwt']
+            req = requests.request(
+                method='POST',
+                url=f"{self.api_url}/user/login",
+                json=payload)
+            req.raise_for_status()
+
+            self._auth_token = req.json()['jwt']
             self.logger.debug('Login succeeded.')
+
         except Exception as exception:
-            self.logger.error(f"Login failed for {user} on {self._site_url}")
-            self.logger.error(f"Details: {exception}")
+            self.logger.error(f'Login failed for {self.account.user} on'
+                              f' {self._site_url}')
+            self.logger.error(f'Details: {exception}')
             sys.exit(1)
 
-    def get_communities(self, type_: str = "Subscribed") -> dict:
-        """Get list of currently subscribed communites"""
-        payload = {
-            'type_': type_,
-            'auth': self._auth_token,
-            'limit': 50,
-            'page': 1
-        }
+    def get_site_response(self) -> None:
+        """Gets the SiteResponse. Will contain MyUserInfo."""
+        self.logger.info('Attempting to get site response.')
+        payload = {'auth': self._auth_token}
 
-        # iterate over each page if needed
-        fetched = 50  # max limit
-        while fetched == 50:
-            try:
-                resp = self._request_it(
-                    f"{self._site_url}/{self._api_base_url}/community/list",
-                    params=payload)
-                fetched = len(resp.json()['communities'])
-                payload['page'] += 1
-
-                for comm in resp.json()['communities']:
-                    id_ = comm['community']['id']
-                    url = comm['community']['actor_id']
-                    self._user_communities[url]['id'] = id_
-            except Exception as err:
-                print(f"error: {err}")
-
-        return self._user_communities
-
-    def subscribe(self, communities: dict = None) -> None:
-        """Subscribe to a community. It will first attempt to resolve community."""
-        if communities:
-            self._user_communities = communities
-        else:
-            self.get_communities()
-
-        payload = {
-            'community_id': None,
-            'follow': True,
-            'auth': self._auth_token
-        }
-
-        for url, _cid in self._user_communities.items():
-            try:
-                # resolve community first
-                comm_id = self.resolve_community(url)
-
-                if comm_id:
-                    payload['community_id'] = comm_id
-                    self._println(2, f"> Subscribing to {url} ({comm_id})")
-                    resp = self._request_it(
-                        f"{self._site_url}/{self._api_base_url}/community/follow",
-                        json=payload, method='POST')
-
-                    if resp.status_code == 200:
-                        self._println(
-                            3, f"> Succesfully subscribed to {url} ({comm_id})")
-            except Exception as exception:
-                print(f"   API error: {exception}")
-
-    def resolve_community(self, community: str) -> int | None:
-        """resolve a community"""
-        payload = {
-            'q': community,
-            'auth': self._auth_token
-        }
-
-        community_id = None
-        self._println(1, f"> Resolving {community}")
         try:
-            resp = self._request_it(
-                f"{self._site_url}/{self._api_base_url}/resolve_object",
+            req = requests.request(
+                method='GET',
+                url=f"{self.api_url}/site",
                 params=payload)
-            community_id = resp.json()['community']['community']['id']
-        except Exception as exception:
-            self._println(2, f"> Failed to resolve community. {exception}")
-
-        return community_id
-
-    def _rate_limit(self):
-        sleep(1)
-
-    def _request_it(self,
-                    endpoint: str,
-                    method: str = 'GET',
-                    params: str = None,
-                    json: dict = None) -> requests.Response:
-
-        self._rate_limit()
-        try:
-            req = requests.request(method,
-                                   url=endpoint,
-                                   params=params,
-                                   json=json)
             req.raise_for_status()
-            return req
-        except requests.exceptions.HTTPError:
-            raise
-        except requests.exceptions.RequestException:
-            raise
 
-    def _println(self, indent, line):
-        print(f"{' ' * indent}{line}")
+        except Exception as error:
+            self.logger.error('Error getting site response.')
+            self.logger.error(f'{error = }')
+            return
+
+        self.logger.info('Site response received. Parsing into object.')
+        self.site_response = json.loads(
+            req.text,
+            object_hook=lambda d: SimpleNamespace(**d))
+
+        self.myuserinfo = MyUserInfo(
+            self.site_response.my_user.local_user_view,
+            self.site_response.my_user.discussion_languages,
+            self.site_response.my_user.follows,
+            self.site_response.my_user.moderates,
+            self.site_response.my_user.community_blocks,
+            self.site_response.my_user.person_blocks)
+
+    def subscribe_to_community(self, community_url: str):
+        """Attempts to subscribe to a community from this instance.
+
+        Args:
+            community_url (str): URL for the community to subscribe to
+        """
+        self.logger.debug(f'Subscribing to "{community_url}"')
+
+        # Check it's not already subscribed to first.
+        for subscribed_community in self.myuserinfo.follows:
+            if community_url in subscribed_community.community.actor_id:
+                self.logger.debug(f'"{community_url}" already subscribed to.')
+                return
+
+        community_id = self.resolve_community_id(community_url=community_url)
+
+        if not community_id:
+            self.logger.error('Unable to resolve Community ID.'
+                              ' Unable to subscribe at this time.')
+            return
+
+        payload = {'community_id': community_id,
+                   'follow': True,
+                   'auth': self._auth_token}
+
+        # TODO - Stopping here for the evening...
+
+    def resolve_community_id(self, community_url: str) -> int | None:
+        """Attempts to resolve the community ID.
+
+        Apparently community IDs are different for each instance?
+
+        Args:
+            community_url (str): URL for the community to subscribe to
+
+        Returns:
+            (int | None): Community ID if it resolved, otherwise None
+        """
+        self.logger.debug(f'Resolving {community_url}')
+        payload = {'q': community_url,
+                   'auth': self._auth_token}
+
+        try:
+            req = requests.request(
+                method='GET',
+                url=f"{self.api_url}/resolve_object",
+                params=payload)
+            req.raise_for_status()
+
+        except Exception as error:
+            self.logger.error('Error resolving community ID.')
+            self.logger.error(f'{error = }')
+            return None
+
+        return req.json()['community']['community']['id']
